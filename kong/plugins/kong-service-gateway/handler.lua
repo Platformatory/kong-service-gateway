9b4934e1d4854e95b487b5e3a7febc65abaadff4
Pavan Keshavamurthy <pavan@grahana.net>
2024-07-21 11:34:09 +0530

    refactor. router and predicate matching.

--- kong/plugins/kong-service-gateway/handler.lua
+++ kong/plugins/kong-service-gateway/handler.lua
@@ -1,10 +1,4 @@
-local cjson = require "cjson"
-local kong = kong
-
-local function is_valid_json(value)
-  local success, _ = pcall(cjson.decode, value)
-  return success
-end
+local url = require "socket.url"
 
 local function get_consumer()
   local consumer = kong.client.get_consumer()
@@ -14,30 +8,78 @@ local function get_consumer()
   return consumer
 end
 
-local function find_kafka_cluster(clusters, domain)
-  for _, cluster in ipairs(clusters) do
-    if cluster.domain == domain then
-      return cluster.bootstrap_servers
-    end
+local function parse_channel_url(channel_url)
+  local parsed_url = url.parse(channel_url)
+  if not parsed_url then
+    return nil, "Invalid channel URL"
   end
-  return nil, "Cluster not found"
+  local uri = {
+    scheme = parsed_url.scheme,
+    host = parsed_url.host,
+    path = {},
+    query = parsed_url.query and ngx.decode_args(parsed_url.query) or nil
+  }
+  for segment in parsed_url.path:gmatch("[^/]+") do
+    table.insert(uri.path, segment)
+  end
+  uri.raw_path = parsed_url.path
+  return uri
 end
 
-local function find_channel(channels, channel_name)
-  for _, channel in ipairs(channels) do
-    if channel.resolve_by == "key_lookup" then
-      local lookup = cjson.decode(channel.value)
-      if lookup[channel_name] then
-        return lookup[channel_name]
+local function match_expression(expression, value)
+  return value:match(expression) ~= nil
+end
+
+local function match_predicates(predicates, uri)
+  for _, predicate in ipairs(predicates) do
+    local part = uri[predicate.part]
+    if predicate.part == "path" and uri.raw_path then
+      part = uri.raw_path
+    end
+    if type(part) == "table" then
+      local matched = false
+      for _, v in ipairs(part) do
+        if match_expression(predicate.expression, v) then
+          matched = true
+          break
+        end
       end
-    elseif channel.resolve_by == "regex" then
-      local match = ngx.re.match(channel_name, channel.value)
-      if match then
-        return match
+      if not matched then
+        kong.log.debug("Predicate did not match: ", predicate.part, " value: ", table.concat(part, ", "), " expression: ", predicate.expression)
+        return false
       end
+    elseif type(part) == "string" then
+      if not match_expression(predicate.expression, part) then
+        kong.log.debug("Predicate did not match: ", predicate.part, " value: ", part, " expression: ", predicate.expression)
+        return false
+      end
+    else
+      kong.log.debug("Predicate did not match: ", predicate.part, " value: nil", " expression: ", predicate.expression)
+      return false
     end
   end
-  return nil, "Channel not found"
+  return true
+end
+
+local function resolve_channel_mappings(rule, uri)
+  if rule.resolve_subject_by.type == "lookup" then
+    local lookup_key_func = load("return " .. rule.resolve_subject_by.lookup_key_function)()
+    local lookup_key = lookup_key_func(uri)
+    return rule.resolve_subject_by.lookup_table[lookup_key]
+  elseif rule.resolve_subject_by.type == "transform" then
+    local transform_func = load("return " .. rule.resolve_subject_by.transform_function)()
+    return transform_func(uri)
+  end
+end
+
+local function find_routing_rule(routing_rules, uri)
+  for _, rule in ipairs(routing_rules) do
+    if match_predicates(rule.predicates, uri) then
+      local channel_mapping = resolve_channel_mappings(rule, uri)
+      return rule, channel_mapping
+    end
+  end
+  return nil, nil, "No matching routing rule found"
 end
 
 local function find_configuration_profile(profiles, profile_name)
@@ -51,28 +93,26 @@ end
 
 local function find_credential(credentials, consumer_name)
   for _, cred in ipairs(credentials) do
-    if cred.consumer_match_type == "LITERAL" and cred.consumer_match_value == consumer_name then
+    if cred.consumer == consumer_name then
       if cred.provider == "literal" then
         return cred.value
       elseif cred.provider == "env" then
-        return os.getenv(cred.value)
-      elseif cred.provider == "JSON_MAP" and is_valid_json(cred.value) then
-        return cjson.decode(cred.value)
-      end
-    elseif cred.consumer_match_type == "REGEX" then
-      local match = ngx.re.match(consumer_name, cred.consumer_match_value)
-      if match then
-        if cred.provider == "literal" then
-          return cred.value
-        elseif cred.provider == "env" then
-          return os.getenv(cred.value)
-        elseif cred.provider == "JSON_MAP" and is_valid_json(cred.value) then
-          return cjson.decode(cred.value)
+        local transformed_value = {}
+        for k, v in pairs(cred.value) do
+          transformed_value[k] = os.getenv(v)
         end
+        return transformed_value
       end
     end
   end
   return nil, "Credential not found"
+end
+
+local function merge_tables(t1, t2)
+  for k, v in pairs(t2) do
+    t1[k] = v
+  end
+  return t1
 end
 
 local KongServiceGateway = {}
@@ -80,58 +120,68 @@ local KongServiceGateway = {}
 KongServiceGateway.VERSION = "1.0.0"
 KongServiceGateway.PRIORITY = 1000
 
-
 function KongServiceGateway:access(config)
+  kong.log.debug("Entered access phase")
+  kong.log.inspect(config)
 
   local consumer, err = get_consumer()
   if not consumer then
+    kong.log.err("Consumer not found: ", err)
     return kong.response.exit(401, { message = err })
   end
 
-  local path = kong.request.get_path()
-  local subpath = string.match(path, "/([^/]+)$")
+  local query_params = kong.request.get_query()
+  kong.log.inspect(query_params)
+  local config_profile = query_params["config_profile"]
+  local channel_url = query_params["channel"]
 
-  if subpath == "kafka_clusters" then
-    local domain = kong.request.get_query()["domain"]
-    if not domain then
-      return kong.response.exit(400, { message = "Domain query parameter is required" })
-    end
-    local result, err = find_kafka_cluster(config.entities.kafka_clusters, domain)
-    if not result then
-      return kong.response.exit(404, { message = err })
-    end
-    return kong.response.exit(200, { bootstrap_servers = result })
-  elseif subpath == "channels" then
-    local channel_name = kong.request.get_query()["channel_name"]
-    if not channel_name then
-      return kong.response.exit(400, { message = "Channel name query parameter is required" })
-    end
-    local result, err = find_channel(config.entities.channels, channel_name)
-    if not result then
-      return kong.response.exit(404, { message = err })
-    end
-    return kong.response.exit(200, { resolved_value = result })
-  elseif subpath == "configuration_profiles" then
-    local profile_name = kong.request.get_query()["profile_name"]
-    if not profile_name then
-      return kong.response.exit(400, { message = "Profile name query parameter is required" })
-    end
-    local result, err = find_configuration_profile(config.entities.configuration_profiles, profile_name)
-    if not result then
-      return kong.response.exit(404, { message = err })
-    end
-    return kong.response.exit(200, { kafka_configurations = result })
-  elseif subpath == "credentials" then
-    local consumer_name = consumer.username or consumer.custom_id
-    local result, err = find_credential(config.entities.credentials, consumer_name)
-    if not result then
-      return kong.response.exit(404, { message = err })
-    end
-    return kong.response.exit(200, { credential_value = result })
-  else
-    return kong.response.exit(404, { message = "Invalid subpath" })
+  if not config_profile or not channel_url then
+    kong.log.err("config_profile or channel query parameters are missing")
+    return kong.response.exit(400, { message = "config_profile and channel query parameters are required" })
   end
+
+  local uri, err = parse_channel_url(channel_url)
+  if not uri then
+    kong.log.err("Invalid channel URL: ", err)
+    return kong.response.exit(400, { message = err })
+  end
+
+  kong.log.inspect(uri)
+
+  -- Iterate over each Kafka cluster
+  for _, kafka_cluster in ipairs(config.inventory.kafka) do
+    -- Check routing rules for this Kafka cluster
+    local rule, channel_mapping, err = find_routing_rule(kafka_cluster.routing_rules, uri)
+    if rule then
+      -- We have a match, find the configuration profile
+      local configuration_profile, err = find_configuration_profile(config.configuration_profiles, config_profile)
+      if not configuration_profile then
+        kong.log.err("Configuration profile not found: ", err)
+        return kong.response.exit(404, { message = err })
+      end
+
+      -- Process the credentials
+      local consumer_name = consumer.username or consumer.custom_id
+      local credential, err = find_credential(config.credentials, consumer_name)
+      if not credential then
+        kong.log.err("Credential not found: ", err)
+        return kong.response.exit(404, { message = err })
+      end
+
+      -- Merge configuration profile and credentials
+      local merged_configuration = merge_tables(configuration_profile, credential)
+
+      -- Prepare the response
+      local response = merge_tables(kafka_cluster.connection, { channel_mapping = channel_mapping, configuration = merged_configuration })
+      kong.log.inspect(response)
+      return kong.response.exit(200, response)
+    else
+      kong.log.debug("No matching routing rule found for cluster: ", kafka_cluster.name, " with host: ", uri.host)
+    end
+  end
+
+  kong.log.err("No matching routing rule found")
+  return kong.response.exit(404, { message = "No matching routing rule found" })
 end
 
 return KongServiceGateway
-
--- kong/plugins/kong-service-gateway/schema.lua
+++ kong/plugins/kong-service-gateway/schema.lua
@@ -1,103 +1,122 @@
-local plugin_name = ({ ... })[1]:match("^kong%.plugins%.([^%.]+)")
 local typedefs = require "kong.db.schema.typedefs"
-
-local typedefs = require "kong.db.schema.typedefs"
-local cjson = require "cjson"
-
--- Function to check if a string is a valid JSON
-local function is_valid_json(value)
-  local success, _ = pcall(function() cjson.decode(value) end)
-  return success
-end
-
--- Custom validator for the `value` field in channels
-local function validate_channel_value(value, config)
-  if config.resolve_by == "key_lookup" then
-    return is_valid_json(value)
-  end
-  return true
-end
 
 return {
   name = "kong-service-gateway",
   fields = {
-    {
+    { 
       config = {
         type = "record",
         fields = {
           {
-            entities = {
+            inventory = {
               type = "record",
               fields = {
                 {
-                  kafka_clusters = {
+                  kafka = {
                     type = "array",
                     elements = {
                       type = "record",
                       fields = {
                         { name = { type = "string", required = true } },
-                        { domain = { type = "string", required = true } },
-                        { tags = { type = "string", required = false } },
-                        { bootstrap_servers = { type = "string", required = true } },
+                        { connection = {
+                            type = "record",
+                            fields = {
+                              { bootstrap_servers = { type = "string", required = true } },
+                            },
+                          },
+                        },
+                        { tags = {
+                            type = "map",
+                            keys = { type = "string" },
+                            values = { type = "string" },
+                            required = false,
+                          },
+                        },
+                        {
+                          routing_rules = {
+                            type = "array",
+                            elements = {
+                              type = "record",
+                              fields = {
+                                {
+                                  predicates = {
+                                    type = "array",
+                                    elements = {
+                                      type = "record",
+                                      fields = {
+                                        { part = { type = "string", required = true } },
+                                        { expression = { type = "string", required = true } },
+                                      },
+                                    },
+                                  },
+                                },
+                                {
+                                  resolve_subject_by = {
+                                    type = "record",
+                                    fields = {
+                                      { type = { type = "string", required = true, one_of = { "lookup", "transform" } } },
+                                      { lookup_key_function = { type = "string", required = false } },
+                                      { lookup_table = {
+                                          type = "map",
+                                          keys = { type = "string" },
+                                          values = { type = "string" },
+                                          required = false,
+                                        },
+                                      },
+                                      { transform_function = { type = "string", required = false } },
+                                    },
+                                  },
+                                },
+                              },
+                            },
+                          },
+                        },
                       },
                     },
                   },
                 },
-                {
-                  channels = {
-                    type = "array",
-                    elements = {
-                      type = "record",
-                      fields = {
-                        { name = { type = "string", required = true } },
-                        { resolve_by = { type = "string", one_of = { "key_lookup", "regex" }, required = true } },
-                        { value = {
-                            type = "string",
-                            required = true,
-                            custom_validator = function(value, config, dao, context)
-                              if context and context.resolve_by == "key_lookup" then
-                                return is_valid_json(value)
-                              end
-                              return true
-                            end,
-                        }},
-                      },
+              },
+            },
+          },
+          {
+            configuration_profiles = {
+              type = "array",
+              elements = {
+                type = "record",
+                fields = {
+                  { name = { type = "string", required = true } },
+                  { kafka_configurations = {
+                      type = "map",
+                      keys = { type = "string" },
+                      values = { type = "string" },
+                      required = true,
+                    },
+                  },
+                  { tags = {
+                      type = "map",
+                      keys = { type = "string" },
+                      values = { type = "string" },
+                      required = false,
                     },
                   },
                 },
-                {
-                  configuration_profiles = {
-                    type = "array",
-                    elements = {
-                      type = "record",
-                      fields = {
-                        { name = { type = "string", required = true } },
-                        { kafka_configurations = {
-                            type = "string",
-                            required = true,
-                            custom_validator = is_valid_json,
-                        }},
-                        { tags = { type = "string", required = false } },
-                      },
-                    },
-                  },
-                },
-                {
-                  credentials = {
-                    type = "array",
-                    elements = {
-                      type = "record",
-                      fields = {
-                        { name = { type = "string", required = true } },
-                        { consumer_match_type = { type = "string", one_of = { "LITERAL", "REGEX" }, required = true } },
-                        { consumer_match_value = { type = "string", required = true } },
-                        { provider = { type = "string", one_of = { "JSON_MAP" }, required = true } },
-                        { value = {
-                            type = "string",
-                            required = true,
-                            custom_validator = is_valid_json,
-                        }},
-                      },
+              },
+            },
+          },
+          {
+            credentials = {
+              type = "array",
+              elements = {
+                type = "record",
+                fields = {
+                  { consumer = { type = "string", required = true } },
+                  { name = { type = "string", required = true } },
+                  { provider = { type = "string", required = true } },
+                  { value = {
+                      type = "map",
+                      keys = { type = "string" },
+                      values = { type = "string" },
+                      required = true,
                     },
                   },
                 },
@@ -109,4 +128,3 @@ return {
     },
   },
 }
-
